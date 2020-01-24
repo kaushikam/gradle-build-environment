434d214b34cac0a16c780d04d4ea1a831f20a3fd
kaushik <kaushikam@gmail.com>
2020-01-24 19:28:38 +0530

    Doc about system property

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,25 @@
-val gradlePropertiesProp: String by project // This comes from the gradle.properties file
-val commandLineValue: String by project // -PcommandLineValue="Value" in command line
-val testProp: String by project // -Dorg.gradle.project.testProp="Value" in command line
-val foo: String by project // systemProp.org.gradle.project.foo=Value in gradle.properties file
-val bar: String by project // export ORG_GRADLE_PROJECT_bar=value in bash
+// This comes from the gradle.properties file
+val gradlePropertiesProp: String by project
+
+// -PcommandLineValue="Value" in command line
+val commandLineValue: String by project
+
+// -Dorg.gradle.project.testProp="Value" in command line
+val testProp: String by project
+
+/*  Properties starting with "systemProp" indicate system properties
+* systemProp.gradle.wrapperUser=kaushik, but the exception is given below
+ */
+
+/*
+* systemProp.org.gradle.project.foo=Value in gradle.properties file. This is a project property
+* In a multi project build, systemProp.” properties set in any project except the project
+* root will be ignored
+ */
+val foo: String by project
+
+// export ORG_GRADLE_PROJECT_bar=value in bash
+val bar: String by project
 
 tasks.register("printProps") {
 	doLast {
